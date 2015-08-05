bf67f75a2bdebdc60942787677d7ecf626de07c8
Abraham Kuri <kurenn@icalialabs.com>
2015-08-05 15:15:23 -0500

    Adds authentication token attribute name option and length

--- lib/generators/toll/templates/toll.rb
+++ lib/generators/toll/templates/toll.rb
@@ -6,4 +6,11 @@ Toll.setup do |config|
 
   # The keys to use for user authentication
   # config.authentication_keys = [:email]
+  #
+
+  # Name of the authentication token attribute for the User
+  # config.authentication_token_attribute_name = :authentication_token
+  
+  # Authentication Token Length
+  # config.authentication_token_length = 64
 end
--- lib/toll.rb
+++ lib/toll.rb
@@ -4,10 +4,26 @@ module Toll
   mattr_accessor :authentication_keys
   @@authentication_keys = [:email]
 
+  # Name of the authentication token attribute for the User
+  mattr_accessor :authentication_token_attribute_name
+  @@authentication_token_attribute_name = :authentication_token
+
+  # Authentication Token Length
+  mattr_accessor :authentication_token_length
+  @@authentication_token_length = 64
+
   # Method to configure Toll
   def self.setup
     yield self
   end
+
+  # Method to generate a Token
+  #
+  def self.token(length = Toll.authentication_token_length)
+    SecureRandom.urlsafe_base64(length)
+  end
 end
 
 require "toll/version"
+require "toll/authenticable"
+require "toll/hooks"
